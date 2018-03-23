59118965af3cae00e7f0bd626337bffcef76d4aa
Eva Antipina <eva.antipina@gmail.com>
2018-03-23 14:19:14 -0700

    Add code to scripts.js file to make page intaractive. It will take input from user, calculate the perfect knowledge track for the user and display it with the user name inserted in the message. The program will ask user to enter their name (alert window) if user forgets to do so.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $("form.survay").submit(function(event){
+
+    var userName = $("input#name").val();
+    var userPriorities = $("input:radio[name=development]:checked").val();
+    var userEnjoy = $("select#enjoy").val();
+    var userDescribes = $("select#describe").val();
+    var userSpareTime = $("select#spareTime").val();
+
+    if (!userName){
+      alert("Please enter your name!");
+      return;
+    };
+
+    if (userEnjoy === '5' && userDescribes === '4' || userPriorities === '5'){
+      $(".addressing").text(userName);
+      $("#notYours").show();
+    } else if(userPriorities === '1' || userPriorities === '4' || userPriorities === '6' && userEnjoy === '1' || userEnjoy === '2'){
+      $(".addressing").text(userName);
+      $("#cssReact").show();
+    } else if(userPriorities === '3' && userEnjoy === '3' || usergoal === '2'){
+      $(".addressing").text(userName);
+      $("#c-NET").show();
+    } else if (userPriorities === '2' && userEnjoy === '3' || userEnjoy === '4' || usergoal === '3') {
+      $(".addressing").text(userName);
+      $("#rubyRails").show();
+    } else if (userPriorities === '6' && userEnjoy === '3' || userEnjoy === '4' || usergoal === '4') {
+      $(".addressing").text(userName);
+      $("#javaAndroid").show();
+    };
+
+    event.preventDefault();
+  });
+});
