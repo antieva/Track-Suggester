67072ae418448a519dc49423f1659a51ec53e6a9
Eva Antipina <eva.antipina@gmail.com>
2018-03-23 17:10:13 -0700

    Final commit: add footer and some additional styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $("form.survay").submit(function(event){
+    //event.preventDefault();
 
     var userName = $("input#name").val();
     var userPriorities = $("input:radio[name=development]:checked").val();
@@ -15,16 +16,21 @@ $(document).ready(function(){
     if (userEnjoy === '5' && userDescribes === '4' || userPriorities === '5'){
       $(".addressing").text(userName);
       $("#notYours").show();
-    } else if(userPriorities === '1' || userPriorities === '4' || userPriorities === '6' && userEnjoy === '1' || userEnjoy === '2'){
+      $(".output").hide();
+    } else if (userPriorities === '1' || userPriorities === '4' || userPriorities === '6' && userEnjoy === '1' || userEnjoy === '2'){
+      $(".output").hide();
       $(".addressing").text(userName);
       $("#cssReact").show();
     } else if(userPriorities === '3' && userEnjoy === '3' || usergoal === '2'){
+      $(".output").hide();
       $(".addressing").text(userName);
       $("#c-NET").show();
     } else if (userPriorities === '2' && userEnjoy === '3' || userEnjoy === '4' || usergoal === '3') {
+      $(".output").hide();
       $(".addressing").text(userName);
       $("#rubyRails").show();
     } else if (userPriorities === '6' && userEnjoy === '3' || userEnjoy === '4' || usergoal === '4') {
+      $(".output").hide();
       $(".addressing").text(userName);
       $("#javaAndroid").show();
     };
